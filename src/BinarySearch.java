fe2fee4ee13501ddaa99438c03a602ed48896711
wangxp_pro <accordionmaster@163.com>
2020-09-11 20:48:26 +0800

    first commit and push

---
+++ src/BinarySearch.java
@@ -0,0 +1,37 @@
+
+
+import java.util.Arrays;
+import edu.princeton.cs.algs4.*;
+
+public class BinarySearch {
+
+	public static int rank(int key, int[] a) {
+		// ��������������
+		int lo = 0;
+		int hi = a.length - 1;
+		while (lo <= hi) {
+			// �����ҵļ�Ҫô�����ڣ�Ҫô��Ȼ������a[lo..hi]֮��
+			int mid = lo + (hi - lo) / 2;
+			if (key < a[mid]) {
+				hi = mid - 1;
+			}else if (key > a[mid]) {
+				lo = mid + 1;
+			}else {
+				return mid;
+			}
+		}
+		return -1;
+	}
+	
+	public static void main(String[] args) {
+		int[] whitelist = In.readInts(args[0]);
+		Arrays.sort(whitelist);
+		while (!StdIn.isEmpty()) {
+			// ��ȡ��ֵ������������ڰ������������ӡ
+			int key = StdIn.readInt();
+			if (rank(key, whitelist) < 0) {
+				StdOut.println(key);
+			}
+		}
+	}
+}
